a353e812b73b8cb85d35b3833b18c4dea95978df
Gopalakrishnan Subramani <92360012+GopalakrishnanSubramani@users.noreply.github.com>
2023-12-03 18:27:06 +0530

    Update build_img.sh

--- build_img.sh
+++ build_img.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-echo "\n*************** Building the Image 1 ***************\n"
+echo "\n*************** Building the Image 1 **************ok*\n"
 
 docker build -t krishgopal37/image_1 ./target_1/
 
