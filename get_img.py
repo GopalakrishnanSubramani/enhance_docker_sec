15dc084374dfd36983ed9452fb9afc3e57dc89b4
Gopalakrishnan Subramani <92360012+GopalakrishnanSubramani@users.noreply.github.com>
2023-12-07 18:26:40 +0530

    Update get_img.py

--- get_img.py
+++ get_img.py
@@ -5,4 +5,3 @@ with open(f"{img_name}",'r') as f:
     line = f.readlines()
     for index,l in enumerate(line):
         docker_images.update({f"image_{index}":l.strip()})
-        print(f"image_{index} -------",l.strip())
